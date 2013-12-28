3ff62a085b6f6499630c27104e348fb56932226d
hailinzeng <hailinzeng@gmail.com>
2013-12-28 15:26:11 +0000

    exercise 1.4, 1.5

---
+++ chapter1/1.5.lisp
@@ -0,0 +1,25 @@
+(define (p) (p))
+
+(define (test x y)
+	(if (= x 0)
+		0
+		y))
+
+(test 0 (p))
+
+;0
+;(if (= 0 0)
+;	0
+;	(p))
+;
+;0
+
+;neverstop
+;(if (= 0 0)
+; 0
+; (p))
+;
+;(if (= 0 0)
+; 0
+; (p))
+;
